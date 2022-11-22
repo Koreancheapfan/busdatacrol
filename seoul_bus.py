e91a3f2bc6a00ef51b87be55204ad3cfdc5ed15f
ssg8697 <ssg8697@naver.com>
2022-11-22 17:08:53 +0900

    서울버스 ver0.2

--- seoul_bus.py
+++ seoul_bus.py
@@ -2,9 +2,10 @@ from bs4 import BeautifulSoup
 import requests
 import datetime
 import json
-info_url = 'httpsbus.go.krxmlRequestgetStationByUid.jspstrBusNumber=23248'
+info_url = 'https://bus.go.kr/xmlRequest/getStationByUid.jsp?strBusNumber=23248'
 response = requests.get(info_url)
 soup = BeautifulSoup(response.content, 'html.parser')
 locations = soup.select('stationList')
-for stationList in locations
-    print(stationList.select_one('rtNm'), stationList.select_one('arrmsg1'), stationList.select_one('arrmsg2'))
+for stationList in locations:
+    print(stationList.select_one('rtNm'), stationList.select_one('arrmsg1'),  stationList.select_one('arrmsg2'))
+
