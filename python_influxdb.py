b6cd1a40243ff162a30d07053ba8135d9f90d9ad
ssg8697 <ssg8697@naver.com>
2022-11-30 15:52:56 +0900

    감사합니다.

--- python_influxdb.py
+++ python_influxdb.py
@@ -6,7 +6,7 @@ import requests
 import lxml
 import time
 import pprint
-import time
+
 
 seq1 = 0
 seq2 = 0
@@ -14,7 +14,7 @@ seq2 = 0
 key = 'Fl+MjXgGrTViuvJTy/RB8zodh2OEXStIUPEaFDSeUHAN5iMIG8zqFW4ROqV//eIee3K9KmL3TdAQQmm4cmnoOQ=='
 rid = '235000092'
 
-def get_ifdb(db, host='54.180.128.66', port=8086, user='ssg', passwd='qwer1234'):
+def get_ifdb(db, host='100.100.100.225', port=8086, user='ssg', passwd='qwer1234'):
     # InfluxDB에 연결하기 위한 정보를 포함하는 객체 생성
     client = InfluxDBClient(host, port, user, passwd, db)
 
@@ -37,12 +37,15 @@ def get_ifdb(db, host='54.180.128.66', port=8086, user='ssg', passwd='qwer1234')
         pass
 
     return client
+Homebuslist = []
+Hometimelist = []
+Awaybuslist = []
+Awaytimelist = []
+bt = 0
 
 for i in range(960):
-    Homebuslist = []
-    Hometimelist = []
-    Awaybuslist = []
-    Awaytimelist = []
+    bt = bt + 1
+
     def bus_data(ifdb):
         url = 'http://apis.data.go.kr/6410000/buslocationservice/getBusLocationList'
         params = {'serviceKey': key, 'routeId': rid}
@@ -50,16 +53,20 @@ for i in range(960):
         soup = BeautifulSoup(response.content, 'xml')
         locations = soup.find_all('busLocationList')
         for busLocationList in locations:
-            global seq1
+            global seq1, dt
             global seq2
+            global bt
             queryTime = soup.select_one('queryTime').text
             routeId = busLocationList.select_one('routeId').text
             stationId = busLocationList.select_one('stationId').text
             stationSeq = busLocationList.select_one('stationSeq').text
             plateNo = busLocationList.select_one('plateNo').text
-            while Hometimelist[0] == '2022-11-29T16:49:20.286253Z'
+
+
+
+
             json_body = []
-            tablename = 'G13002'
+            tablename = 'G1300'
             fieldname = 'zero'
             point = {"measurement": tablename,
                      "tags": {
@@ -76,7 +83,7 @@ for i in range(960):
                      }
             vals = list(range(1))
             for v in vals:
-                dt = datetime.now()  # 한국시간으로 설정하기위해 UTC에서 9시간을 더함
+                dt = datetime.now()
                 np = deepcopy(point)
                 np['fields'][fieldname] = v
                 np['time'] = dt
@@ -88,69 +95,86 @@ for i in range(960):
 
 
 
-            if stationId == "277103549":    #만약 기점 정류장이면 기점 정류장에있는 버스 정보 저장
-                seq1 = seq1 + 1
-                json_body = []
-                tablename = 'HOMEG1300'
-                fieldname = 'zero'
-                point = {"measurement": tablename,
-                         "tags": {
-                             "routeId": routeId,
-                             "stationId": stationId,
-                             "stationSeq": stationSeq,
-                             "plateNo": plateNo,
-                             "homeseqnum": seq1
-                         },
-                         "fields": {
-                             # Initialize data to zero
-                             fieldname: 0
-                         },
-                         "time": None,
-                         }
-                vals = list(range(1))
-                for v in vals:
-                    dt = datetime.now() + timedelta(hours=9)  # 한국시간으로 설정하기위해 UTC에서 9시간을 더함
-                    np = deepcopy(point)
-                    np['fields'][fieldname] = v
-                    np['time'] = dt
-                    json_body.append(np)
+            if len(Hometimelist) != 0:
+                if bt > Hometimelist[0] + 20:
+                    del Homebuslist[0]
+                    del Hometimelist[0]
+                    # Homebuslist.remove(0)
+                    # Hometimelist.remove(0)
+            if len(Awaytimelist) != 0:
+                if bt > Awaytimelist[0] + 20:
+                    del Awaybuslist[0]
+                    del Awaytimelist[0]
+                    # Awaybuslist.remove(0)
+                    # Awaytimelist.remove(0)
 
 
-                ifdb.write_points(json_body)  # json_body에 저장
-                result = ifdb.query('select * from %s' % tablename)
-                pprint.pprint(result.raw)
+            if len(Homebuslist) == 0 or plateNo not in Homebuslist:
+                if stationId == "277102386":  # 만약 기점 정류장이면 기점 정류장에있는 버스 정보 저장
+                    Homebuslist.append(plateNo)
+                    Hometimelist.append(bt)
+                    seq1 = seq1 + 1
+                    json_body = []
+                    tablename = 'HOMEG1300'
+                    fieldname = 'zero'
+                    point = {"measurement": tablename,
+                             "tags": {
+                                 "routeId": routeId,
+                                 "stationId": stationId,
+                                 "stationSeq": stationSeq,
+                                 "plateNo": plateNo,
+                                 "homeseqnum": seq1
+                             },
+                             "fields": {
+                                 # Initialize data to zero
+                                 fieldname: 0
+                             },
+                             "time": None,
+                             }
+                    vals = list(range(1))
+                    for v in vals:
+                        dt = datetime.now()
+                        np = deepcopy(point)
+                        np['fields'][fieldname] = v
+                        np['time'] = dt
+                        json_body.append(np)
 
-            elif stationId == "123000611":  #만약 종점 정류장이면 종점 정류장에있는 버스 정보 저장
-                seq2 = seq2 + 1
-                json_body = []
-                tablename = 'AWAYG1300'
-                fieldname = 'zero'
-                point = {"measurement": tablename,
-                         "tags": {
-                             "routeId": routeId,
-                             "stationId": stationId,
-                             "stationSeq": stationSeq,
-                             "plateNo": plateNo,
-                             "awayseqnum": seq2
-                         },
-                         "fields": {
-                             # Initialize data to zero
-                             fieldname: 0
-                         },
-                         "time": None,
-                         }
-                vals = list(range(1))
-                for v in vals:
-                    dt = datetime.now() + timedelta(hours=9)  # 한국시간으로 설정하기위해 UTC에서 9시간을 더함
-                    np = deepcopy(point)
-                    np['fields'][fieldname] = v
-                    np['time'] = dt
-                    json_body.append(np)
+                    ifdb.write_points(json_body)  # json_body에 저장
+                    result = ifdb.query('select * from %s' % tablename)
+                    pprint.pprint(result.raw)
+            if len(Awaybuslist) == 0 or plateNo not in Homebuslist:
+                if stationId == "123000611" and plateNo not in Awaybuslist:  # 만약 기점 정류장이면 기점 정류장에있는 버스 정보 저장
+                    Awaybuslist.append(plateNo)
+                    Awaytimelist.append(bt)  # 만약 종점 정류장이면 종점 정류장에있는 버스 정보 저장
+                    seq2 = seq2 + 1
+                    json_body = []
+                    tablename = 'AWAYG1300'
+                    fieldname = 'zero'
+                    point = {"measurement": tablename,
+                             "tags": {
+                                 "routeId": routeId,
+                                 "stationId": stationId,
+                                 "stationSeq": stationSeq,
+                                 "plateNo": plateNo,
+                                 "awayseqnum": seq2
+                             },
+                             "fields": {
+                                 # Initialize data to zero
+                                 fieldname: 0
+                             },
+                             "time": None,
+                             }
+                    vals = list(range(1))
+                    for v in vals:
+                        dt = datetime.now()
+                        np = deepcopy(point)
+                        np['fields'][fieldname] = v
+                        np['time'] = dt
+                        json_body.append(np)
 
-
-                ifdb.write_points(json_body)  # json_body에 저장
-                result = ifdb.query('select * from %s' % tablename)
-                pprint.pprint(result.raw)
+                    ifdb.write_points(json_body)  # json_body에 저장
+                    result = ifdb.query('select * from %s' % tablename)
+                    pprint.pprint(result.raw)
 
     def data_save():
         # InfluxDB에 접속
@@ -164,4 +188,15 @@ for i in range(960):
         data_save()
 
     print("====데이터 추출완료====")
+    print(len(Homebuslist))
+    print(len(Awaybuslist))
+    print(Awaybuslist)
+    print(Awaytimelist)
+    if len(Awaytimelist) != 0:
+        print(Awaytimelist[0])
+    # print(dt)
+    # ct = datetime.now() + timedelta(seconds=1200)
+    # print(ct)
+    # print(queryTime)
     time.sleep(60)
+
